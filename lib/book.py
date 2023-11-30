cde2227cae4b35ab567052a69e2f68042cb40e47
michael <michaelngige@gmail.com>
2023-11-30 08:01:14 +0300

    passing tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count:int):
+        assert page_count , "page_count must be an integer"
+
+        self.title = title
+        self.page_count = page_count
+    def turn_page(self):
+        return "Flipping the page...wow,you read fast!"
+
+my_book= Book("fathers of nations", 20)
+
+print(my_book.title)
+print(my_book.page_count)
+print(my_book.turn_page())
+    
+     
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size: int):
+        self.brand = brand
+        assert isinstance(size, int), "Size must be an integer"
+        self.size = size
+        self.condition = 'New'  
+
+    def repair(self):
+        self.condition = 'Repaired'
+        self.cobble = True
+
+
+try:
+    my_shoe = Shoe('adidas', '20') 
+except AssertionError as e:
+    print(e)
+
+# Test repair method
+my_shoe = Shoe('nike', 10)
+print(my_shoe.condition)  # Output: New
+
+my_shoe.repair()
+print(my_shoe.condition)  # Output: Repaired
+print(my_shoe.cobble)  # Output: True
